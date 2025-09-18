7735074cfbb2126b16578ca400c4bad2f5382cc8
Matej Kraljevic <193736737+matej04-dot@users.noreply.github.com>
2025-09-18 12:47:26 +0200

    fixes for vercel 12

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
 import Login from "@/components/Login";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <Login />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
